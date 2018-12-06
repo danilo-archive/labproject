65849894d22852dd1e19210076ee8956f5b3fda9
Mahum Hashmi <35971248+Mahum13@users.noreply.github.com>
2018-12-06 10:38:49 +0000

    Updated query_functions.php
    Added the SQL queries.

--- query_functions.php
+++ query_functions.php
@@ -1,12 +1,27 @@
 <?php
 function load_all_games() {
+
+	$games = "SELECT name, platform, artwork, genre, rating, description, releaseDate, developer FROM GameCopy, Game";
+	$load_all_games_set = mysqli_query(videogame_db, $games);
+	return $load_all_games_set;
 }
-function get_games_genre($id) {}
+function get_games_genre($genre) { 
+	$game_by_genre = "SELECT name, platform, artwork, genre, rating, description, releaseDate, developer FROM GameCopy, Game WHERE Game.gameID = GameCopy.gameID AND genre = "{$genre}"";
+	$game_genre_set = mysqlil_query($db, $game_by_genre);
+	return $game_genre_set;
+
+}
 
 function count_genre($id){
   $result = get_games_genre($id);
   mysqli_num_rows($result);
 }
-function get_rating($game) {}
+
+function get_game_console($console) {
+	$game_console = "SELECT name, platform, artwork, genre, rating, description, releaseDate, developer FROM GameCopy, Game WHERE Game.gameID = GameCopy.gameID AND console = "{$console}"";
+	$game_console_set = mysqli_query($db, $game_console);
+	return $game_console_set;
+}
+
   
- ?>
+?>
