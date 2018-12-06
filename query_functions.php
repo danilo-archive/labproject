69ef782e5fede948958ce39d76a3ed19e6c19f12
Mahum Hashmi <35971248+Mahum13@users.noreply.github.com>
2018-12-06 10:42:05 +0000

    Updated query_functions.php

--- query_functions.php
+++ query_functions.php
@@ -1,11 +1,12 @@
 <?php
 function load_all_games() {
-
+	gloabl $db;
 	$games = "SELECT name, platform, artwork, genre, rating, description, releaseDate, developer FROM GameCopy, Game";
 	$load_all_games_set = mysqli_query(videogame_db, $games);
 	return $load_all_games_set;
 }
 function get_games_genre($genre) { 
+	gloabl $db;
 	$game_by_genre = "SELECT name, platform, artwork, genre, rating, description, releaseDate, developer FROM GameCopy, Game WHERE Game.gameID = GameCopy.gameID AND genre = "{$genre}"";
 	$game_genre_set = mysqlil_query($db, $game_by_genre);
 	return $game_genre_set;
@@ -13,11 +14,13 @@ function get_games_genre($genre) {
 }
 
 function count_genre($id){
+	global $db;
   $result = get_games_genre($id);
   mysqli_num_rows($result);
 }
 
 function get_game_console($console) {
+	global $db;
 	$game_console = "SELECT name, platform, artwork, genre, rating, description, releaseDate, developer FROM GameCopy, Game WHERE Game.gameID = GameCopy.gameID AND console = "{$console}"";
 	$game_console_set = mysqli_query($db, $game_console);
 	return $game_console_set;
