f542e047e174b62726d3a1cc105d202d72114eec
Hannah Paulding <hannah.paulding@icloud.com>
2018-12-06 10:11:32 +0000

    Add queries.sql with skeleton queries for game and genre

--- private/setup/queries.sql
+++ private/setup/queries.sql
@@ -2,7 +2,15 @@
 select name, platform, artwork, genre, rating, description, releaseDate, developer from GameCopy, Game
 where Game.gameID = GameCopy.gameID;
 
-/*Query to list all racing games*/
-select name, platform, artwork, genre, rating, description, releaseDate, developer from GameCopy, Game 
-where Game.gameID = GameCopy.gameID AND genre = "Racing";
+/*Query to list games by genre*/
 
+select name, platform, artwork, genre, rating, description, releaseDate, developer from GameCopy, Game 
+where Game.gameID = GameCopy.gameID AND genre = "...";
+
+/*the query is the same depending on the genre - you can edit this via php*/
+
+/*Query to list games by console*/
+select name, platform, artwork, genre, rating, description, releaseDate, developer from GameCopy, Game 
+where Game.gameID = GameCopy.gameID AND console = "...";
+
+/*the query is the same depending on the console - you can edit this via php too*/
