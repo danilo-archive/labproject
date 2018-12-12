b5e6a57544e324229595f3283fb548f378d0e097
sofiestubo <k1763988@kcl.ac.uk>
2018-12-12 18:05:24 +0000

    make it poss to see imges

--- gameinfo.php
+++ gameinfo.php
@@ -18,7 +18,7 @@ echo "</ul>";
 echo "<div class='product product__style--3 col-lg-4 col-md-4 col-sm-6 col-12'>";
 echo "  <div class='product__thumb'>";
 echo "    <!--Change data target to reflect the right details modal-->";
-echo "    <a class='first__img' data-target='#modalID' data-toggle='modal' href='single-product.html'><img alt='product image' src=$image></a>";
+    echo "    <a class='first__img' data-target='#modalID' data-toggle='modal' href='single-product.html'><img alt='product image' src='images/game_icons/{$image}'></a>";
 echo "  </div>";
 echo "  <div class='product__content content--center'>";
 echo "    <h4><a href='single-product.html'>$title</a></h4>";
