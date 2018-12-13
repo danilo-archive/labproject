a870c02dc3563058cb40f4eefaf4fd815ce03019
Hannah Paulding <hannah.paulding@icloud.com>
2018-12-13 11:43:41 +0000

    Add queries to queries.sql to produce a list of the games available to rent

--- private/setup/queries.sql
+++ private/setup/queries.sql
@@ -16,3 +16,47 @@ where Game.gameID = GameCopy.gameID AND console = "...";
 /**show games by genre*/
 SELECT genre, COUNT(*) FROM Game GROUP BY genre;
 
+select GameCopy.copyID, gameID, platform, damageValue from GameCopy, Rental
+where Rental.copyID=GameCopy.copyID;
+
+select * from GameCopy;
+
+select distinct a.c1, a.c2 
+from a
+where not exists (
+    select 1 from b
+    where b.c1 = a.c1
+      and b.c2 = a.c2
+)
+
+select GameCopy.copyID, gameID, platform, damageValue from GameCopy, Rental
+where Rental.copyID=GameCopy.copyID
+EXCEPT
+select GameCopy.copyID, gameID, platform, damageValue from GameCopy;
+
+
+/*query to select all available GameCopies i.e. the query will list the 
+copyIDs for all game copies that have NOT been rented*/
+
+select GameCopy.copyID from GameCopy
+where GameCopy.copyID not in
+(select GameCopy.copyID from GameCopy, Rental
+where Rental.copyID=GameCopy.copyID);
+
+/*This will produce all gameCopy fields for all game copies that are currently available
+e.g. copyID | gameID | platform | damageValue |
+for ALL games available to rent*/
+
+select GameCopy.copyID, gameID, platform, damageValue from GameCopy,
+    (select GameCopy.copyID from GameCopy
+    where GameCopy.copyID not in
+        (select GameCopy.copyID from GameCopy, Rental
+        where Rental.copyID=GameCopy.copyID)) as AvailableGames
+ where GameCopy.copyID = AvailableGames.copyID;
+
+
+
+
+
+
+
