d18d5f0a0eada73eb228817c1be8203a1b4002fc
hannahpaulding <hannah.paulding@icloud.com>
2018-12-13 09:25:07 +0000

    Update populate.sql to populate the database

--- private/setup/populate.sql
+++ private/setup/populate.sql
@@ -1,21 +1,40 @@
-insert into Game(gameID, name, artwork, genre, rating, description, releaseDate, developer, price)
-    values(1, "MarioKart", "someimg.txt", "Racing", 5, "this is a description lol", "2003-01-02", "Nintendo", "49");
-    
-insert into GameCopy(copyID, gameID, platform, damageValue)
-    values(1, 1, "Nintendo Switch", 42.50);
-    
 insert into Member(memberId, fname, lname, email)
     values (1, "Hannah", "Paulding", "hannah.paulding@email.com");
-    
-insert into Rental (rentalID, copyID, memberID, dateBorrowed, dateDue)
-    values (1,1,1,'2018-12-1', '2018-12-22');
-    
+
+insert into Member(memberId, fname, lname, email)
+    values (2, "Danilo", "Del Busso", "danilo@email.com");
+
+insert into Member(memberId, fname, lname, email)
+    values (3, "Maren-Sofie", "Stubo", "maren-sofie@email.com");
+
+insert into Member(memberId, fname, lname, email)
+    values (4, "Mahum", "Hashmi", "mahum@email.com");
+
+insert into Member(memberId, fname, lname, email)
+    values (5, "John", "Smith", "john@email.com");
+
+
 insert into Game(gameID, name, artwork, genre, rating, description, releaseDate, developer, price)
-    values(1, "MarioKart", "someimg.txt", "Racing", 5, "this is a description lol", "2003-01-02", "Nintendo", "49");
-    
-/* Members */
-insert into Member(memberId, fname, lname, email)
-    values (1, "Hannah", "Paulding", "hannah.paulding@email.com");
-    
-insert into Member(memberId, fname, lname, email)
-    values (2, "", "Paulding", "hannah.paulding@email.com");
+    values(1, "Counter-Strike: Global Offensive", "1.jpg", "Action", 5, 
+"Counter-Strike: Global Offensive (CS: GO) will expand upon the team-based action gameplay that it pioneered when it was launched 14 years ago. CS: GO features new maps, characters, and weapons and delivers updated versions of the classic CS content (de_dust2, etc.)",
+"2012–08-21", "Valve", "15");
+
+insert into Game(gameID, name, artwork, genre, rating, description, releaseDate, developer, price)
+    values(2, "Europa Universalis IV", "2.jpg", "Strategy", 4, 
+"The empire building game Europa Universalis IV gives you control of a nation to guide through the years in order to create a dominant global empire. Rule your nation through the centuries, with unparalleled freedom, depth and historical accuracy.",
+"2013–08-13", "Paradox Development Studio", "40");
+
+insert into Game(gameID, name, artwork, genre, rating, description, releaseDate, developer, price)
+    values(3, "DARK SOULS™: Prepare To Die Edition", "3.jpg", "Action", 5, 
+"Dark Souls will be the most deeply challenging game you play this year. Can you live through a million deaths and earn your legacy?",
+"2012–08-24", "FromSoftware", "40");
+
+insert into Game(gameID, name, artwork, genre, rating, description, releaseDate, developer, price)
+    values(4, "The Elder Scrolls V: Skyrim", "4.jpg", "RPG", 1, 
+"EPIC FANTASY REBORN The next chapter in the highly anticipated Elder Scrolls saga arrives from the makers of the 2006 and 2008 Games of the Year, Bethesda Game Studios. Skyrim reimagines and revolutionizes the open-world fantasy epic, bringing to life a complete virtual world open for you to explore any way you choose.",
+"2011–11-10", "Bethesda Game Studios", "20");
+
+insert into Game(gameID, name, artwork, genre, rating, description, releaseDate, developer, price)
+    values(5, "DARK SOULS™ III", "5.jpg", "FromSoftware", 3, 
+"Dark Souls continues to push the boundaries with the latest, ambitious chapter in the critically-acclaimed and genre-defining series. Prepare yourself and Embrace The Darkness!",
+"2016–04-11", "FromSoftware", "20");
