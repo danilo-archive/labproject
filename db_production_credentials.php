004a828ac7eed2cb99bd81a6190a0c8a79538c00
Hannah Paulding <hannah.paulding@icloud.com>
2018-12-14 19:01:53 +0000

    Add additional queries and update the database structure

---
+++ db_production_credentials.php
@@ -0,0 +1,7 @@
+<?php
+
+    define("DB_SERVER", "fdb24.awardspace.net");
+    define("DB_USER", "2909927_videogame");
+    define("DB_PASS", "passwordindigo2018");
+    define("DB_NAME", "2909927_videogame");
+    ?>
